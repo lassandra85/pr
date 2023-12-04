903edcbdabf0af6262082c17402b0703ed9af65b
lassandra85 <lassandra85@gmail.com>
2023-12-04 19:02:23 +0100

    task-1

---
+++ js/task-01.js
@@ -0,0 +1,14 @@
+const categories = document.querySelector("#categories");
+
+console.log(`Number of categories:  ${categories.children.length}`);
+
+const classNames = document.querySelectorAll(".item");
+
+classNames.forEach(function(childName) {
+    console.log(`Category: ${childName.firstElementChild.textContent}
+    Elements: ${childName.lastElementChild.children.length} `);
+}); 
+
+
+
+
