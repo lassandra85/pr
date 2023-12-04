8282d1d155d4ce3d5b702712a9c52c85cafaba98
lassandra85 <lassandra85@gmail.com>
2023-12-05 00:05:51 +0100

    task-2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,13 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsItems = ingredients.map(ingredientsItem => {
+  const ingredient = document.createElement('li');
+  ingredient.classList.add("item");
+  ingredient.textContent = `${ingredientsItem}`;
+  return ingredient;
+});
+
+const ingredientsList = document.querySelector("#ingredients");
+ingredientsList.append(...ingredientsItems);
