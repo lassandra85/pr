5a946a9cde76f81a798f6218ce1a503c20ff814a
lassandra85 <lassandra85@gmail.com>
2023-12-11 19:54:21 +0100

    task-6

--- js/task-05.js
+++ js/task-05.js
@@ -2,3 +2,20 @@
 
 <input type="text" id="name-input" placeholder="Please enter your name" />
 <h1>Hello, <span id="name-output">Anonymous</span>!</h1> */
+
+const ref = {
+    inputText: document.querySelector('#name-input'),
+    nameOutput: document.querySelector('#name-output'),
+};
+
+const changeName = (e) => {
+    /* if (ref.textContent.length === 0) {
+        
+    } */
+    e.preventDefault();
+    console.log("input")
+};
+
+ref.inputText.addEventListener('input', changeName);
+
+
