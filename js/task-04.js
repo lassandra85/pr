aa3772e28c0c7d37a2cb9502ca94b9685baa36bd
lassandra85 <lassandra85@gmail.com>
2024-01-20 00:04:19 +0100

    task-4

--- js/task-04.js
+++ js/task-04.js
@@ -1,16 +1,19 @@
-const counterValue = document.querySelector('#value');
+const counterValue = document.querySelector("#value");
 counterValue.textContent = 0;
 
-const buttonDecrement = document.querySelector('button[data-action="decrement"]');
-const onClickbuttonDecrement = buttonDecrement.addEventListener('click', () => {counterValue.textContent = Number(counterValue.textContent) - 1} );
+const buttonDecrement = document.querySelector(
+  'button[data-action="decrement"]'
+);
+const onClickbuttonDecrement = buttonDecrement.addEventListener("click", () => {
+  counterValue.textContent = Number(counterValue.textContent) - 1;
+});
 
-const buttonIncrement = document.querySelector('button[data-action="increment"]');
-const onClickbuttonIncrement = buttonIncrement.addEventListener('click', () => {counterValue.textContent = Number(counterValue.textContent) + 1} );
- 
-
-
-
-
+const buttonIncrement = document.querySelector(
+  'button[data-action="increment"]'
+);
+const onClickbuttonIncrement = buttonIncrement.addEventListener("click", () => {
+  counterValue.textContent = Number(counterValue.textContent) + 1;
+});
 
 /* Счетчик состоит из спана и кнопок, которые, при клике, должны увеличивать и уменьшать его значение на единицу.
 
