1525dafb504dd8ff3112291faa2af56bd98ca255
lassandra85 <lassandra85@gmail.com>
2024-01-19 23:35:22 +0100

    task -2

--- js/task-02.js
+++ js/task-02.js
@@ -1,18 +1,18 @@
-/* const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+const ingredients = [
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
 
-const ingredientsItems = ingredients.map(ingredientsItem => {
-  const ingredient = document.createElement('li');
+const ingredientsItems = ingredients.map((ingredientsItem) => {
+  const ingredient = document.createElement("li");
   ingredient.classList.add("item");
   ingredient.textContent = `${ingredientsItem}`;
   return ingredient;
 });
 
 const ingredientsList = document.querySelector("#ingredients");
-ingredientsList.append(...ingredientsItems); */
+ingredientsList.append(...ingredientsItems);
