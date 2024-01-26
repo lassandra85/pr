39c8a8387cc6c9f5dc17ab7fea59f09cfbd446db
lassandra85 <lassandra85@gmail.com>
2024-01-26 22:43:19 +0100

    task-10

--- js/task-10.js
+++ js/task-10.js
@@ -1,9 +1,9 @@
-function getRandomHexColor() {
+/* function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
-
+ */
 /* Напиши скрипт создания и очистки коллекции элементов. Пользователь вводит количество элементов в input и нажимает кнопку Создать, после чего рендерится коллекция. При нажатии на кнопку Очистить, коллекция элементов очищается.
 
 <div id="controls">
@@ -19,10 +19,5 @@ function getRandomHexColor() {
 Размеры самого первого <div> - 30px на 30px.
 Каждый элемент после первого, должен быть шире и выше предыдущего на 10px.
 Все элементы должены иметь случайный цвет фона в формате HEX. Используй готовую функцию getRandomHexColor для получения цвета.
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
-}
 
 Создай функцию destroyBoxes(), которая очищает содержимое div#boxes, тем самым удаляя все созданные элементы. */
