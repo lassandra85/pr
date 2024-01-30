c20cb8557c5ad5880504f115f8ca26436477fee9
lassandra85 <lassandra85@gmail.com>
2024-01-30 23:55:29 +0100

    task-7

--- js/task-07.js
+++ js/task-07.js
@@ -5,3 +5,10 @@
 <span id="text">Abracadabra!</span> */
 
 const inputRange = document.querySelector("#font-size-control");
+const text = document.querySelector("#text");
+
+inputRange.addEventListener("input", changeFontSize);
+
+function changeFontSize(evt) {
+  text.style.fontSize = `${evt.currentTarget.value}px`;
+}
