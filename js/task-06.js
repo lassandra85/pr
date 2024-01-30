b1eba55bc7692b362e1c4b71023634dced2029fa
lassandra85 <lassandra85@gmail.com>
2024-01-31 00:10:00 +0100

    task-6

--- js/task-06.js
+++ js/task-06.js
@@ -22,3 +22,14 @@
 #validation-input.invalid {
   border-color: #f44336;
 } */
+const textInput = document.querySelector("#validation-input");
+
+const inputChange = textInput.addEventListener("change", inputValue);
+
+function inputValue(e) {
+  if (textInput.value.length >= textInput.data - set.length) {
+    textInput.id = validation - input.valid;
+  } else {
+    textInput.id = validation - input.invalid;
+  }
+}
