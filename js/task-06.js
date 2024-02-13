4394fd0ef5b7bf2e2fef9ee8870e460a96b17c0f
lassandra85 <lassandra85@gmail.com>
2024-02-13 23:03:36 +0100

    task-6

--- js/task-06.js
+++ js/task-06.js
@@ -3,9 +3,11 @@ const textInput = document.querySelector("#validation-input");
 const inputChange = textInput.addEventListener("blur", inputValue);
 
 function inputValue(e) {
-  if (textInput.value.length >= textInput.data - set.length) {
-    textInput.id = validation - input.valid;
+  if (textInput.value.length < textInput.dataset.length) {
+    textInput.classList.remove("valid");
+    textInput.classList.add("invalid");
   } else {
-    textInput.id = validation - input.invalid;
+    textInput.classList.remove("invalid");
+    textInput.classList.add("valid");
   }
 }
