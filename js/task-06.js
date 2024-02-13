f94c4cc86f5017d14a96f12a1745b6e835e7b5a5
lassandra85 <lassandra85@gmail.com>
2024-02-13 22:54:45 +0100

    task-6

--- js/task-06.js
+++ js/task-06.js
@@ -1,14 +1,11 @@
 const textInput = document.querySelector("#validation-input");
 
-const inputChange = textInput.addEventListener("change", inputValue);
+const inputChange = textInput.addEventListener("blur", inputValue);
 
 function inputValue(e) {
-  if (e.current.value.length >= textInput.data - set.length) {
+  if (textInput.value.length >= textInput.data - set.length) {
     textInput.id = validation - input.valid;
   } else {
     textInput.id = validation - input.invalid;
   }
-  /*   if (e.currentTarget.value.length >= .data - set.length) {
-    
-  } */
 }
