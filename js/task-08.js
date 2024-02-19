95cfe1ed896aeac1ebe5c5bad0ff73fdd62489c3
lassandra85 <lassandra85@gmail.com>
2024-02-19 14:56:56 +0100

    task-8

--- js/task-08.js
+++ js/task-08.js
@@ -6,10 +6,10 @@ loginForm.addEventListener("submit", submitForm);
 function submitForm(event) {
   event.preventDefault();
 
-  const form = event.target;
-  const { email, password } = form.elements;
+  const { email, password } = event.target.elements;
 
   console.log(email.value);
+  console.log(password.value);
 
   if (email.value === "" || password.value === "") {
     alert("Please fill in all the fields!");
@@ -24,9 +24,3 @@ function submitForm(event) {
 
   form.reset();
 }
-
-/* Обработка отправки формы form.login-form должна быть по событию submit.
-При отправке формы страница не должна перезагружаться.
-Если в форме есть незаполненные поля, выводи alert с предупреждением о том, что все поля должны быть заполнены.
-Если пользователь заполнил все поля и отправил форму, собери значения полей в обьект, где имя поля будет именем свойства, а значение поля - значением свойства. Для доступа к элементам формы используй свойство elements.
-Выведи обьект с введенными данными в консоль и очисти значения полей формы методом reset. */
