9afcfa2554541c336d3c9b48a59dd3fb674dacbc
lassandra85 <lassandra85@gmail.com>
2024-02-21 00:01:13 +0100

    task-10

--- js/task-10.js
+++ js/task-10.js
@@ -1,12 +1,60 @@
-/* function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
-} */
 const controls = document.querySelector("#controls");
 console.dir(controls);
 
-const inputNumber = controls.firstElementChild;
+const boxes = document.querySelector("#boxes");
+console.dir(boxes);
+
+const inputNumber = document.querySelector('input[type="number"]');
+console.dir(inputNumber);
+
 const buttonCreate = document.querySelector("button[data-create");
 console.dir(buttonCreate);
-/* controls.button[data - create] */
+
+const buttonDestroy = document.querySelector("button[data-destroy");
+console.dir(buttonDestroy);
+
+buttonCreate.addEventListener("click", () => createBoxes(inputNumber.value));
+
+buttonDestroy.addEventListener("click", destroyBoxes);
+
+function createBoxes(amount) {
+  if (amount >= +inputNumber.min && amount <= +inputNumber.max) {
+    /*====    Варіант за методом createElement()    ====*/
+    /*const boxesAmount = [];
+     for (let i = 0; i < amount; i += 1) {
+     
+      const box = document.createElement("div");
+      box.style.backgroundColor = getRandomHexColor();
+      box.style.width = `${30 + 10 * i}px`;
+      box.style.height = `${30 + 10 * i}px`;
+      boxesAmount.push(box);
+     }
+
+     boxes.append(...boxesAmount); */
+    /*====    Варіант за методом insertAdjacentHTML()    ====*/
+
+    let boxesAmount = "";
+
+    for (let i = 0; i < amount; i += 1) {
+      boxesAmount += `<div style = 
+      "background-color: ${getRandomHexColor()}; 
+      width: ${30 + 10 * i}px; 
+      height: ${30 + 10 * i}px;">
+      </div>`;
+    }
+    boxes.insertAdjacentHTML("afterbegin", boxesAmount);
+  } else {
+    alert("Please enter number in the form!");
+    return;
+  }
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+function destroyBoxes() {
+  boxes.innerHTML = "";
+}
