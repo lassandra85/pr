06c726c518881de3c95f048d4e36720cadfe43a7
lassandra85 <lassandra85@gmail.com>
2024-02-26 22:35:52 +0100

    reset

--- js/task-07.js
+++
@@ -1,8 +0,0 @@
-const inputRange = document.querySelector("#font-size-control");
-const text = document.querySelector("#text");
-
-inputRange.addEventListener("input", changeFontSize);
-
-function changeFontSize(evt) {
-  text.style.fontSize = `${evt.currentTarget.value}px`;
-}
