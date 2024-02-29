4a74afdaea3786902abafd4e89d0a2d121a92c74
lassandra85 <lassandra85@gmail.com>
2024-02-29 01:22:42 +0100

    01-galleryJS

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,24 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+console.dir(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+
+const markup = galleryItems
+  .map(
+    ({ preview, original, description }) => `<li class="gallery__item">
+  <a class="gallery__link" href = ${original}>
+    <img
+      class = "gallery__image"
+      src = ${preview}
+      data-source = ${original}
+      alt = ${description}
+    />
+  </a>
+</li>`
+  )
+  .join("");
+
+gallery.insertAdjacentHTML("beforeend", markup);
+console.dir(gallery);
