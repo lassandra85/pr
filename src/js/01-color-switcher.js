da9aa8089c87a7568c5b70738db795fedcda0cdf
lassandra85 <lassandra85@gmail.com>
2024-04-30 09:19:46 +0200

    color-switch

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,9 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
 
+startBtn.addEventListener('click', startBtnClickHandler);
+stopBtn.addEventListener('click', stopBtnClickHandler);
+
+function startBtnClickHandler(event) {}
+function startBtnClickHandler(event) {}
+/* console.dir(start); */
