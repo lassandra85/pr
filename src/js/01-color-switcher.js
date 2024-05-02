4aa933ee956f2651b3e2d87330e5f451cdfbaffb
lassandra85 <lassandra85@gmail.com>
2024-05-02 12:06:06 +0200

    add

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,10 +1,33 @@
 const startBtn = document.querySelector('button[data-start]');
 const stopBtn = document.querySelector('button[data-stop]');
+const bodyElement = document.body;
+let setIntervalID = null;
+/* console.dir(startBtn); */
 
-const startBtnClickHandler = event => {};
-const stopBtnClickHandler = event => {};
+stopBtn.disabled = true;
+
+const startBtnClickHandler = () => {
+  randomBody();
+  setIntervalID = setInterval(randomBody, 1000);
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
+};
+
+const stopBtnClickHandler = () => {
+  clearInterval(setIntervalID);
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
+};
+
+function randomBody() {
+  bodyElement.style.backgroundColor = getRandomHexColor();
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
 
 startBtn.addEventListener('click', startBtnClickHandler);
 stopBtn.addEventListener('click', stopBtnClickHandler);
-
-/* console.dir(start); */
