ec1bda1e5e61182ac2a736e09e0504c8f7b2e269
lassandra85 <lassandra85@gmail.com>
2024-06-04 22:43:33 +0200

    03

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -13,3 +13,45 @@ createPromise(2, 1500)
   .catch(({ position, delay }) => {
     console.log(`❌ Rejected promise ${position} in ${delay}ms`);
   });
+
+/* import { Notify } from 'notiflix';
+
+const promisesForm = document.querySelector('.form');
+promisesForm.addEventListener('submit', onPromisesFormSubmit);
+
+function onPromisesFormSubmit(e) {
+  e.preventDefault();
+  const { amount, delay, step } = e.target.elements;
+  const submitButton = e.target.lastElementChild;
+  let delayTime = Number(delay.value);
+  
+  submitButton.disabled = true;
+  for (let i = 1; i <= amount.value; i += 1) {
+    createPromise(i, delayTime)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+    delayTime += Number(step.value);
+  }
+
+  setTimeout(() => {
+    submitButton.disabled = false;
+  }, delayTime);
+}
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+ */
