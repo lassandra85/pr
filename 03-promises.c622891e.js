6d4c2f4988c8f14a8224b1a436c870007a1bed15
lassandra85 <105219220+lassandra85@users.noreply.github.com>
2024-06-14 20:25:28 +0000

    Deploying to gh-pages from @ lassandra85/pr@0f5c187010ad6de822fc2d90409224fef560859c 🚀

---
+++ 03-promises.c622891e.js
@@ -0,0 +1,2 @@
+!function(){document.querySelector(".form");(void Math.random()).then((function(o){var n=o.position,c=o.delay;console.log("✅ Fulfilled promise ".concat(n," in ").concat(c,"ms"))})).catch((function(o){var n=o.position,c=o.delay;console.log("❌ Rejected promise ".concat(n," in ").concat(c,"ms"))}))}();
+//# sourceMappingURL=03-promises.c622891e.js.map
