5e3a4f079fba6dcbfa924288dd774bc8ab366e32
lassandra85 <lassandra85@gmail.com>
2024-07-02 22:54:00 +0200

    03

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -13,14 +13,14 @@ function onPromisesFormSubmit(e) {
     });
 }
 
-function createPromise(position, delay) {
+/* function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
     // Fulfill
   } else {
     // Reject
   }
-}
+} */
 
 /* 
 
