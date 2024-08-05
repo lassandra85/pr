df0aba26ce44055b39d6827c7ea9cc48cd3e2ec9
lassandra85 <105219220+lassandra85@users.noreply.github.com>
2024-08-05 20:02:04 +0000

    Deploying to gh-pages from @ lassandra85/pr@7e4ea5b68805b46beee3a8937979e0c3206e1f4f 🚀

---
+++ 01-color-switcher.03d500b0.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=document.body,a=null;e.disabled=!0;function d(){n.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}t.addEventListener("click",(function(){d(),a=setInterval(d,1e3),t.disabled=!0,e.disabled=!1})),e.addEventListener("click",(function(){clearInterval(a),t.disabled=!1,e.disabled=!0}))}();
+//# sourceMappingURL=01-color-switcher.03d500b0.js.map
